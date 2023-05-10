a5123b395887b51d74d77defba43d6555942bb12
Akshat Sikarwar <asikarwar1@bloomberg.net>
2023-05-10 18:09:28 -0400

    Tweak script to be more like TestDelete_ForcedContention

--- bench.lua
+++ bench.lua
@@ -1,29 +1,51 @@
-#!/home/asikarw1/src/bench/build/luacdb2
-
-total_rows = 256
-thds = 48
-dbs = {}
+#!/home/asikarw1/src/luacdb2/build/luacdb2
 
 function connect()
     local dbname = "akdb"
     local tier = "local"
     if #arg >= 1 then dbname = arg[1] end
     if #arg >= 2 then tier = arg[2] end
-    print("dbname:"..dbname)
-    print("tier:"..tier)
     for i = 1, thds do
         table.insert(dbs, comdb2(dbname, tier))
+        db = dbs[#dbs]
+        db:rd_stmt("select comdb2_dbname(), comdb2_host(), comdb2_semver()")
+        print(string.format("thd:%2d   dbname:%s/%s   host:%s   version:%s", i, db:column_value(1), tier, db:column_value(2), db:column_value(3)))
+        db:drain()
     end
-    db = dbs[1]
 end
 
 function truncate()
-    db:wr_stmt("create table if not exists t(i integer)")
-    db:wr_stmt("truncate table t")
+    print('truncate')
+    db:wr_stmt("drop table if exists t")
+    --db:wr_stmt("create table if not exists t(i integer unique)")
+    db:wr_stmt("create table if not exists t(i integer unique, j integer unique, k integer unique)")
+    --db:wr_stmt("truncate table t")
+end
+
+function tbl_stats()
+    db:wr_stmt("put tunable parallel_count 1")
+    db:rd_stmt("select min(i), max(i), count(i) from t")
+    print("min:".. db:column_value(1) .. " max:"..  db:column_value(2) .. " count:" .. db:column_value(3))
+    db:drain()
 end
 
 function insert(total)
+    print('insert')
     if total < thds then error("not enough data to insert") end
+
+    if total < 100000 then
+        db:wr_stmt("set transaction chunk 10000")
+        db:wr_stmt("begin")
+        db:wr_stmt(string.format("insert into t(i) select value from generate_series(1, %d)", total))
+        db:wr_stmt("commit")
+        --for i = 1, total, 10000 do
+        --    local ins = string.format("insert into t(i) select value from generate_series(%d, %d)", i, i + 10000 - 1)
+        --    print(ins)
+        --    db:wr_stmt(ins)
+        --end
+        return
+    end
+
     if math.fmod(total, thds) ~= 0 then error("need count divisible by " .. thds) end
     for i = 1, thds do
         dbs[i]:wr_stmt("set transaction chunk " .. 10000)
@@ -34,10 +56,10 @@ function insert(total)
     local per_thd = total / thds
     for i = 1, thds do
         to = from + per_thd - 1;
-        print("thd:"..i.." from:"..from.." to:"..to)
+        --print("thd:"..i.." from:"..from.." to:"..to)
         dbs[i]:bind(1, from)
         dbs[i]:bind(2, from + per_thd - 1)
-        dbs[i]:async_stmt("insert into t select value from generate_series(?, ?)")
+        dbs[i]:async_stmt("insert into t(i) select value from generate_series(?, ?)")
         from = to + 1
     end
     for i = 1, thds do
@@ -54,43 +76,24 @@ end
 
 function verify_err()
     for i = 1, thds do
-        dbs[i]:async_stmt("update t set i = i + 1")
+        dbs[i]:async_stmt("delete from t where i >= 1 limit 5000")
     end
     for i = 1, thds do
         if dbs[i]:verify_err() then num_verify = num_verify + 1 end
     end
 end
 
-function scon()
-    db:rd_stmt("exec procedure sys.cmd.send('scon')")
-    db:drain()
-end
-
 function test()
+    num_verify = 0
+    thds = 10
+    dbs = {}
     connect()
     truncate()
-    print("thds:"..thds)
-    local ins = "insert into t select value from generate_series(1, " .. tostring(total_rows) ..")"
-    print(ins)
-    db:wr_stmt(ins)
-    scon()
-
-    local num_verify = 0
-    local count = 32
-    local comma = ""
-    for i = 1, count do
-        io.write(comma, i)
-        io.flush()
-        comma=","
-        verify_err()
-    end
-    io.write("\n")
+    insert(60000)
+    tbl_stats()
+    verify_err()
     print("failed:"..num_verify)
-    print("expected min, max:".. tostring(thds * count + 1) ..", ".. tostring(thds * count + total_rows))
-
-    db:rd_stmt("select min(i), max(i), count(i) from t")
-    print("actual min, max:".. db:column_value(1) .. "," .. db:column_value(2))
-    db:drain()
+    tbl_stats()
 end
 
 test()
